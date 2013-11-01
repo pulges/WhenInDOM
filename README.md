af3248b63da047da2c798b2bde6490c1e482cdbe
Oliver Pulges <oliver@fraktal.ee>
2013-11-01 09:43:35 +0200

    Make the basic code

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 WhenInDOM
 =========
 
-A library agnostic javascript function for listening when node gets inserted to DOM
+A library agnostic javascript function for listening when node gets inserted to DOM.
+Should work in older browsers too, but will be slower as cannot use mutationObservers.
+
+## Usage
+    
+    // Lets say you have a div not rendered
+    var a = document.createElement('div');
+    
+    // you can listen to when it gets rendered like this
+    var myObserver = new WhenInDOM(a, function () {
+      alert('Got inserted to dom');
+    });
+    
+    // lets insert it
+    setTimeout(function () {
+        document.body.appendChild(a);
+    }, 1000);
+    
+When observing should be stopped before element has recieved in DOM, the following should be done:
+    
+    myObserver.stop();
+    
+
+## Happy birthday Mikk Pristavka!
+
+Hope it will cheer You up more than once.
---
+++ whenInDom.js
@@ -0,0 +1,76 @@
+var WhenInDOM = function (el, callback, observeEl) {
+    var MObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+    
+    this.observer = null;
+    this.callback = callback;
+    this.el = el;
+    this.observeEl = observeEl || document;
+
+    var nodeInList = function (list, node) {
+        for (var i = list.length; i--;) {
+            if (list[i] === node || contains(list[i], node)) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    var contains = function (container, element) {
+        var documentElement = document.documentElement;
+        if (documentElement.contains) {
+            if (element.nodeType !== 1) {
+                element = element.parentNode;
+            }
+            return container !== element && container.contains(element);
+        } else if (documentElement.compareDocumentPosition) {
+            return !!(container.compareDocumentPosition(element) & 16);
+        }
+    };
+
+    this.init = function () {
+        var that = this;
+        var config = {
+            childList: true,
+            subtree: true
+        };
+        this.observer = new MObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                if (mutation.type == "childList" && nodeInList(mutation.addedNodes, that.el)) {
+                    that.stop();
+                    that.callback();
+                }
+            });
+        });
+        this.observer.observe(this.observeEl, config);
+    };
+    
+    this.crawl = function() {
+        if (this.crawlEl && this.crawlEl.parentNode) {
+            if (this.crawlEl.parentNode === this.observeEl) {
+                this.stop();
+                this.callback();
+            } else {
+                this.crawlEl = this.crawlEl.parentNode;
+            }
+        }
+    };
+    
+    this.crawlInit = function() {
+        var that = this;
+        this.crawlEl = this.el;
+        this.crawlHandler = setInterval(function () {
+            that.crawl.call(that);
+        }, 10);
+    };
+
+    this.stop = function () {
+        if (this.observer) { this.observer.disconnect(); }
+        if (this.crawlHandler) { clearInterval(this.crawlHandler); }
+    };
+    
+    if (MObserver) {
+        this.init();
+    } else {
+        this.crawlInit();
+    }
+};
